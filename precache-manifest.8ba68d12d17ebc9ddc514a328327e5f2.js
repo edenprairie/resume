959722a244ac2f26b033b36da6013e5a1ecca877
edenprairie <edenprairie@users.noreply.github.com>
2020-12-06 16:03:45 +0000

    deploy: 7ca22fcb1ed9f74de29b241964a3370efbc73fee

---
+++ precache-manifest.8ba68d12d17ebc9ddc514a328327e5f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d4ab8ada99ce2703b6fefbb2daba81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e44447ca5bdce67f585b",
+    "url": "/static/css/main.f9af6252.chunk.css"
+  },
+  {
+    "revision": "856fd554b450bd43d529",
+    "url": "/static/js/2.25cb1671.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.25cb1671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44447ca5bdce67f585b",
+    "url": "/static/js/main.477529a5.chunk.js"
+  },
+  {
+    "revision": "1d8dc7023c717ddbf896",
+    "url": "/static/js/runtime-main.7fcbc3aa.js"
+  },
+  {
+    "revision": "778887085776d0777d6f764ad8f9428d",
+    "url": "/static/media/JunWangResume.77888708.pdf"
+  },
+  {
+    "revision": "8597c8aabe5bc6e74c800cb78842421c",
+    "url": "/static/media/junwang.8597c8aa.jpg"
+  }
+]);
